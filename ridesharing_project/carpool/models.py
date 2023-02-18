fe8a5d81015fe2e7ae03247c4c037c19c2260570
Max Duchesne <max.duchesne@gmail.com>
2023-02-17 20:17:01 -0500

    ride capacity > 0

--- ridesharing_project/carpool/models.py
+++ ridesharing_project/carpool/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.exceptions import ValidationError
+
+def validate_positive(value):
+    if value <= 0:
+        raise ValidationError(
+            ('You must have at least one spot in your car to post a ride')
+        )
 
 class Ride(models.Model):
     origin = models.CharField(max_length=100)
@@ -9,8 +16,9 @@ class Ride(models.Model):
     #departure_time = choices for different times of day
     driver = models.ForeignKey(User, on_delete=models.CASCADE)
     notes = models.TextField(blank=True)
-    capacity = models.IntegerField()
+    capacity = models.PositiveIntegerField()
     num_riders = models.IntegerField()
+
     
     # should create __str__ method to name rides so they aren't "ride object"
     
