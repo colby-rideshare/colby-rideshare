4a401702e4945090d2d07c464a4239800b2c531f
Max Duchesne <max.duchesne@gmail.com>
2023-02-18 20:39:29 -0500

    gas prices api and added GasPrice model

--- ridesharing_project/carpool/admin.py
+++ ridesharing_project/carpool/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
-from .models import Ride
+from .models import Ride, GasPrice
 
 admin.site.register(Ride)
-
-
-
+admin.site.register(GasPrice)
--- ridesharing_project/carpool/models.py
+++ ridesharing_project/carpool/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 
-
+# don't need this anymore but there is issue with exisiting db data when deleting
 def validate_positive(value):
     if value <= 0:
         raise ValidationError(
@@ -15,14 +15,23 @@ class Ride(models.Model):
     destination = models.CharField(max_length=100)
     departure_day = models.DateField()
     time = models.CharField(max_length = 100)
-    #departure_time = choices for different times of day
     driver = models.ForeignKey(User, on_delete=models.CASCADE)
     notes = models.TextField(blank=True)
     capacity = models.PositiveIntegerField()
     num_riders = models.IntegerField()
+    #add float field for estimated gas costs (miles / mpg * current gas prices)
 
     
     # should create __str__ method to name rides so they aren't "ride object"
     
     def get_absolute_url(self):
         return reverse('ride-detail',kwargs={'pk':self.pk})  #gets url of specific ride detail page
+    
+#the purpose of this is to essentially serve as a timer
+#current_time is updated every time a user visits the rides page
+#fetch_gas_price is set to one week after the last time we fetched gas prices
+#every time we fetch gas price, we reset fetch_gas_price
+class GasPrice(models.Model):
+    last_update = models.DateTimeField()
+    next_update = models.DateTimeField()
+    gas_price = models.FloatField()
