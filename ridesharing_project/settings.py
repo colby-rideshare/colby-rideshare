0051842870f2f717ef4065270bc85505bbe8cc96
Max Duchesne <max.duchesne@gmail.com>
2023-03-07 19:17:45 -0500

    more testing on settings.py debug

--- ridesharing_project/settings.py
+++ ridesharing_project/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'False')
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['carpool-app.herokuapp.com']
 
