d232232e9de8825b9212e4392c0014ee4a6f4360
Max Duchesne <max.duchesne@gmail.com>
2023-03-08 21:36:07 -0500

    changed welcome email to have new domain

--- users/views.py
+++ users/views.py
@@ -31,8 +31,8 @@ def register(request):
                 f"Whether you're looking for a ride to Portland Jetport or just looking for people to keep you company on a long drive back to New York, " \
                 f"we hope that Colby Rideshare will bring us closer as a community and ease some of the stress that many students have when making travel plans to and from Colby. " \
                 f"Colby Rideshare is completely free to use, although riders are encouraged to chip in for drivers' gas costs.\n\n" \
-                f"To get started, simply log in at https://carpool-app.herokuapp.com/ with your username and password. From there, you can post an upcoming ride if you are driving or search for other ride offers. " \
-                f"Please don't hesitate to reach out to our support team at https://carpool-app.herokuapp.com/support/ if you have any questions, issues, or suggestions on how we can improve. " \
+                f"To get started, simply log in at https://www.colbyrideshare.live with your username and password. From there, you can post an upcoming ride if you are driving or search for other ride offers. " \
+                f"Please don't hesitate to reach out to our support team at https://www.colbyrideshare.live/support/ if you have any questions, issues, or suggestions on how we can improve. " \
                 f"Thanks again for using Colby Rideshare and please help other students find rides by encouraging fellow Mules to join!" \
                 f"\n\nBest,\nThe Colby Rideshare Team"
             from_email = os.environ.get('EMAIL_USER')
